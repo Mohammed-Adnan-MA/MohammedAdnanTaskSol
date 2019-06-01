ab3337bc05fab28e54e11f42078e26dd8b8c1bdc
Mohammed Adnan <21203283+Mohammed-Adnan-MA@users.noreply.github.com>
2019-06-01 23:25:16 +0300

    Add files via upload

---
+++ MohammedAdnanTaskSol/MohammedAdnanTasSol/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MohammedAdnanTasSol
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine(Solution.solution(new int[0]));
+            Console.ReadLine();
+		}
+	}
+}
